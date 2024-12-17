3536b677b3373eded918e75aec67f46d62017186
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2024-12-17 11:25:56 +0000

    release: 0.4.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = []
 
 setup(
     name="libcoveweb2",
-    version="0.3.0",
+    version="0.4.0",
     author="Open Data Services",
     author_email="code@opendataservices.coop",
     packages=find_packages(),
